a78ea8ec341dae29c6bc75ff9dc1819d58544001
SouthernRebs <fordviper2012@yahoo.com>
2018-04-10 23:57:55 -0500

    Added DbSchema

---
+++ app/src/main/java/com/androidchatapp/MessyDbSchema.java
@@ -0,0 +1,29 @@
+package com.androidchatapp;
+
+public class MessyDbSchema {
+    public static final class ProfileTable {
+        public static final String NAME = "profile";
+
+        public static final class Profile {
+            public static final String UUID = "uuid";
+            public static final String USERID = "userid";
+            public static final String PASSWORD = "password";
+            public static final String MESSAGE = "message";
+            public static final String SENT = "sent";
+            public static final String NICKNAME = "nickname";
+            public static final String PIC = "profilepic";
+        }
+    }
+
+    public static final class MessagesTable {
+        public static final String NAME = "messages";
+
+        public static final class Messages {
+            public static final String UUID = "uuid";
+            public static final String NICKNAME = "nickname";
+            public static final String SENT = "sentstatus";
+            public static final String TIME = "timesent";
+            public static final String LOCATION = "location";
+        }
+    }
+}
